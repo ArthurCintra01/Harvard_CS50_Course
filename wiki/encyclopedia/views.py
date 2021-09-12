b0681b46448b8ff1b7b9875b71f9a93d62f86121
ArthurCintra01 <turcko.cintra@gmail.com>
2021-09-12 01:39:57 -0300

    wiki

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django import forms
+import markdown2
 
 from . import util
 
@@ -8,3 +10,14 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entryName):
+    en = util.get_entry(entryName)
+    if en == None:
+        html = markdown2.markdown("# Error\n \tYour requested page was not found")
+    else:
+        html = markdown2.markdown(en)
+    with open('encyclopedia/templates/encyclopedia/temp.html', 'w') as f:
+        f.write(html)
+    return render(request, "encyclopedia/entry.html",{
+        "name" : entryName
+    })

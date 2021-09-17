8c43b6a9a3420129e64e6a0f34bef8e1ed972a4b
ArthurCintra01 <turcko.cintra@gmail.com>
2021-09-16 23:58:21 -0300

    sql

--- SQL/airline/flights/views.py
+++ SQL/airline/flights/views.py
@@ -6,3 +6,9 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render (request, "flights/flight.html",{
+        "flight": flight
+    })

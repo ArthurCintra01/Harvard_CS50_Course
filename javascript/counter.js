8dd7c24dcb621b12e7290f90de075e379488597c
ArthurCintra01 <turcko.cintra@gmail.com>
2021-09-30 12:13:34 -0300

    currency-api

--- javascript/counter.js
+++ javascript/counter.js
@@ -1,12 +1,16 @@
-let counter =0;
-    function count() {
-        counter++;
-        document.querySelector('h1').innerHTML = counter;
-        if (counter % 10 === 0){
-            alert(`Count is now ${counter}`);
-        }
-    }
-    document.addEventListener('DOMContentLoaded', function(){
-        //document.addEventListener('click', count);
-        document.querySelector('button').onclick = count;
-    });
+
+
+if (!localStorage.getItem('counter')){
+    localStorage.setItem('counter', 0);
+}
+
+function count() {
+    let counter = localStorage.getItem('counter');
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter);
+}
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter')
+    document.querySelector('button').onclick = count;
+});

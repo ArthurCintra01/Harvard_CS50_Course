ee9c7787c4b038d855f5197ba288798cd5dd33c6
ArthurCintra01 <turcko.cintra@gmail.com>
2021-10-08 14:54:31 -0300

    ci/cd

--- CI-CD/selenium/tests.py
+++ CI-CD/selenium/tests.py
@@ -9,7 +9,7 @@ def file_uri(filename):
     return pathlib.Path(os.path.abspath(filename)).as_uri()
 
 
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 
 
 class WebpageTests(unittest.TestCase):
